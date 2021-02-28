818d59cd0a5e3c79bc694342d8f5a089e226e71e
uly <UlyanaTsi@users.noreply.github.com>
2021-03-01 01:00:51 +0300

    Initial Commit

---
+++ KindCare/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  KindCare
+//
+//  Created by Ульяна Цимбалистая on 01.03.2021.
+//  Copyright © 2021 Ульяна Цимбалистая. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
