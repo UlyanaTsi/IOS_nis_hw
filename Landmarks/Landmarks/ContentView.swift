bc9588978590fbf0c96d477b0356f91a06c3a859
uly <60287888+UlyanaTsi@users.noreply.github.com>
2021-07-11 20:36:02 +0300

    Add files via upload

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ульяна Цимбалистая on 07.02.2021.
+//  Copyright © 2021 Ульяна Цимбалистая. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
